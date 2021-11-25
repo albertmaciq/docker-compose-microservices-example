ed5228d98db7962ae28d734336041a12b95adf35
Alberto Maciques Nieto <alberto.maciques@vass.es>
2021-11-25 13:51:02 +0100

    adding home endpoint with logger for test ekl implementation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,23 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class DemoApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
+
+	@RequestMapping("/")
+	String home() {
+		LOGGER.info("Endpoint for home had been called");
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
