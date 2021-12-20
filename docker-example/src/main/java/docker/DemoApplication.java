50b828599a709812eddea0a5bf526d6f445f8542
Alberto Maciques Nieto <alberto.maciques@vass.es>
2021-12-20 17:42:00 +0100

    updating project with some features checkstyle files,jmeter module,etc

--- docker-example/src/main/java/docker/DemoApplication.java
+++ docker-example/src/main/java/docker/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.docker;
+package docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(final String... args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+  public static void main(final String... args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 }
--- docker-example/src/main/java/docker/infraestructure/rest/ControllerDemo.java
+++ docker-example/src/main/java/docker/infraestructure/rest/ControllerDemo.java
@@ -1,13 +1,12 @@
-package com.example.docker.infraestructure.rest;
+package docker.infraestructure.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerDemo {
-    @GetMapping("/")
-    public String home() {
-        return "Hello World!";
-    }
-
+  @GetMapping("/")
+  public String home() {
+    return "Hello World!";
+  }
 }
